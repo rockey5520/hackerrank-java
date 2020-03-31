b7f4f334d605a346c3df74bfcc62e71837c283b4
Rakesh Mothukuri <rockey5520@gmail.com>
2020-03-31 17:18:38 +0100

    sock merchant solution

--- interviewprepararionkit/warmupchallenges/sockmerchant/Solution.java
+++ interviewprepararionkit/warmupchallenges/sockmerchant/Solution.java
@@ -1,48 +1,46 @@
-package interviewpreparationkit.warmupchallenges.sockmerchant;
+package warmupchallenges.sockmerchant;
 
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Solution {
 
     // Complete the sockMerchant function below.
     static int sockMerchant(int n, int[] ar) {
-
-        return 1;
-
-
+        Map<Integer, Integer> m = new HashMap<>();
+        for (int a : ar) {
+            int pointer = a;
+            int count = 0;
+            for (int b : ar) {
+                if (b == a) {
+                    count++;
+                }
+                m.put(a, count);
+            }
+        }
+        int numberofpairs = 0;
+        for (int x : m.values()
+        ) {
+            int numberofpairs_tmp = 0;
+            if (x > 1) {
+                if (x % 2 == 0) {
+                    numberofpairs_tmp = x / 2;
+                } else {
+                    numberofpairs_tmp = (x - 1) / 2;
+                }
+            }
+            numberofpairs = numberofpairs + numberofpairs_tmp;
+        }
+        return numberofpairs;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        int[] a = new int[]{10, 20, 20, 10, 10, 30, 50, 10, 20};
+        System.out.println(sockMerchant(9, a));
 
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        int[] ar = new int[n];
-
-        String[] arItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        for (int i = 0; i < n; i++) {
-            int arItem = Integer.parseInt(arItems[i]);
-            ar[i] = arItem;
-        }
-
-        int result = sockMerchant(n, ar);
-
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedWriter.close();
-
-        scanner.close();
     }
 }
