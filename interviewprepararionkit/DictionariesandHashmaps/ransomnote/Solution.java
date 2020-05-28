e73420569a4eea7f28ece42dec79daca2ac5779f
Rakesh Mothukuri <rockey5520@gmail.com>
2020-05-28 15:33:01 +0100

    Ransom note

---
+++ interviewprepararionkit/DictionariesandHashmaps/ransomnote/Solution.java
@@ -0,0 +1,70 @@
+package DictionariesandHashmaps.ransomnote;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+  // Complete the checkMagazine function below.
+  static void checkMagazine(String[] magazine, String[] note) {
+    if (note.length > magazine.length) {
+      System.out.println("No");
+      System.exit(0);
+    }
+    Map<String, Integer> magazineMap;
+    Map<String, Integer> noteMap;
+
+    magazineMap = count(magazine);
+    noteMap = count(note);
+    System.out.println(magazineMap.toString());
+    System.out.println(noteMap.toString());
+
+    Set<String> strings = noteMap.keySet();
+    for (String s : strings
+    ) {
+      System.out.println(s);
+      System.out.println(magazineMap.get(s));
+      if (magazineMap.containsKey(s)) {
+        if (noteMap.get(s) <= magazineMap.get(s)) {
+          continue;
+        } else {
+          System.out.println("No");
+          System.exit(0);
+        }
+      }else{
+        System.out.println("No");
+        System.exit(0);
+      }
+    }
+    System.out.println("Yes");
+  }
+
+  private static Map<String, Integer> count(String[] word) {
+    Map<String, Integer> map = new HashMap<>();
+    for (String s : word
+    ) {
+      if (!map.containsKey(s)) {
+        map.put(s, 1);
+      } else {
+        map.put(s, map.get(s) + 1);
+      }
+    }
+    return map;
+  }
+
+  private static final Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) {
+
+    String[] magazine = "ive got a lovely bunch of coconuts".split(" ");
+    String[] note = "ive got some coconuts".split(" ");
+
+    checkMagazine(magazine, note);
+  }
+}
