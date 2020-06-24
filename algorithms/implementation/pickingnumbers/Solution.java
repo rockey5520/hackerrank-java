f26b8e626d8f3241a3faeb566b3b98808c6a6207
Rakesh Mothukuri <rockey5520@gmail.com>
2020-06-24 21:56:38 +0100

    picking numbers Solution.java and README.md

--- algorithms/implementation/pickingnumbers/Solution.java
+++ algorithms/implementation/pickingnumbers/Solution.java
@@ -1,10 +1,25 @@
 package implementation.pickingnumbers;
 
+import java.util.List;
+
 public class Solution {
 
+  public static int pickingNumbers(List<Integer> a) {
+    int[] frequency = new int[101];
+    for (Integer i : a) {
+      frequency[i]++;
+    }
+    int maxLength = 0;
+    for (int i = 1; i < frequency.length - 1; i++) {
+      if ((frequency[i] + frequency[i + 1]) > maxLength) {
+        maxLength = frequency[i] + frequency[i + 1];
+      }
+    }
+    return maxLength;
+  }
 
   public static void main(String[] args) {
-
+    System.out.println(pickingNumbers(List.of(1, 1, 2, 2, 4, 4, 5, 5, 5)));
   }
 
 }
