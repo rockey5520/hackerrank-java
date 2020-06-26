e669da4f40b018b4b131ae50fb665d33edfce882
Rakesh Mothukuri <rockey5520@gmail.com>
2020-06-26 17:07:42 +0100

    Library fine Solution.java and README.md

--- algorithms/implementation/libraryfine/Solution.java
+++ algorithms/implementation/libraryfine/Solution.java
@@ -2,9 +2,22 @@ package implementation.libraryfine;
 
 public class Solution {
 
+  static int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2) {
+    if (y1 > y2) {
+      return 10000;
+    }
+    if ((m1 > m2) && (y1 >= y2)) {
+      return (m1 - m2) * 500;
+    }
+    if ((d1 > d2) && (m1 >= m2) && (y1 >= y2)) {
+      return (d1 - d2) * 15;
+    }
+    return 0;
+  }
+
 
   public static void main(String[] args) {
-
+    libraryFine(9, 6, 2015, 6, 6, 2015);
   }
 
 }
