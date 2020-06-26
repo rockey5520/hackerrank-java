b6c1c62fa8298fdaa888540646d649b4195d510b
Rakesh Mothukuri <rockey5520@gmail.com>
2020-06-26 09:49:32 +0100

    Utopian tree Solution.java and README.md

--- algorithms/implementation/utopiantree/Solution.java
+++ algorithms/implementation/utopiantree/Solution.java
@@ -4,10 +4,24 @@ public class Solution {
 
   static int utopianTree(int n) {
 
+    int growth = 1;
+    if (n == 0) {
+      return growth;
+    }
+    for (int i = 1; i <= n; i++) {
+      if (i % 2 == 0) {
+        growth += 1;
+      } else {
+        growth *= 2;
+      }
+    }
+    return growth;
+
   }
 
   public static void main(String[] args) {
-
+    int i = utopianTree(4);
+    System.out.println(i);
   }
 
 }
