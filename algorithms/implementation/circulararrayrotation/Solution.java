33dc86e2a1fe3c0b46a1b2e1dd2de614e14e77f1
Rakesh Mothukuri <rockey5520@gmail.com>
2020-06-26 13:11:48 +0100

    Circular array rotation Solution.java and README.md

--- algorithms/implementation/circulararrayrotation/Solution.java
+++ algorithms/implementation/circulararrayrotation/Solution.java
@@ -2,8 +2,24 @@ package implementation.circulararrayrotation;
 
 public class Solution {
 
-  public static void main(String[] args) {
+  static int[] circularArrayRotation(int[] a, int k, int[] queries) {
+    int[] result = new int[queries.length];
+    int[] rotated = new int[a.length];
 
+    for (int i = 0; i < a.length; i++) {
+      int newPosition = (i + k) % (a.length);
+      rotated[newPosition] = a[i];
+    }
+
+    for (int x = 0; x < queries.length; x++) {
+      result[x] = rotated[queries[x]];
+    }
+
+    return result;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(circularArrayRotation(new int[]{3, 4, 5}, 1, new int[]{1, 2}));
   }
 
 }
