ad127a5fb215556562ef700f9452ae81248676b1
Rakesh Mothukuri <rockey5520@gmail.com>
2020-06-26 10:44:54 +0100

    Angry Professor Solution.java and README.md

--- algorithms/implementation/angryprofessor/Solution.java
+++ algorithms/implementation/angryprofessor/Solution.java
@@ -3,11 +3,22 @@ package implementation.angryprofessor;
 public class Solution {
 
   static String angryProfessor(int k, int[] a) {
+    int lateStudents = 0;
+    for (int value : a) {
+      if (value <= 0) {
+        lateStudents++;
+      }
+    }
 
+    if (lateStudents >= k) {
+      return "NO";
+    } else {
+      return "YES";
+    }
   }
 
   public static void main(String[] args) {
-
+    System.out.println(angryProfessor(2, new int[]{0, -1, 2, 1}));
   }
 
 }
