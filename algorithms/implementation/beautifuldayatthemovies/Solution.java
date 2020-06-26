9fc45975448ef50ec15ed2e9d5edf47b2ea0f97a
Rakesh Mothukuri <rockey5520@gmail.com>
2020-06-26 12:04:06 +0100

    Beautiful day at the movies Solution.java and README.md

--- algorithms/implementation/beautifuldayatthemovies/Solution.java
+++ algorithms/implementation/beautifuldayatthemovies/Solution.java
@@ -2,8 +2,30 @@ package implementation.beautifuldayatthemovies;
 
 public class Solution {
 
-  public static void main(String[] args) {
+  static int beautifulDays(int i, int j, int k) {
+    int beautifulDays = 0;
+    for (int d = i; d <= j; d++) {
+      if (Math.abs(d - reverseInteger(d)) % k == 0) {
+        beautifulDays++;
+      }
+    }
+    return beautifulDays;
 
+  }
+
+  private static int reverseInteger(Integer integer) {
+    int reverse = 0;
+    while (integer > 0) {
+      reverse = (reverse * 10) + (integer % 10);
+      integer /= 10;
+    }
+
+    return reverse;
+  }
+
+
+  public static void main(String[] args) {
+    System.out.println(beautifulDays(20, 23, 6));
   }
 
 }
