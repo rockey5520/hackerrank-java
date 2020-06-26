962b330c3ff4f85f1e62590fdb63fa0504ee0e18
Rakesh Mothukuri <rockey5520@gmail.com>
2020-06-26 17:38:16 +0100

    cut the sticks Solution.java and README.md

--- algorithms/implementation/cutthesticks/Solution.java
+++ algorithms/implementation/cutthesticks/Solution.java
@@ -1,9 +1,29 @@
 package implementation.cutthesticks;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Solution {
 
-  public static void main(String[] args) {
+  static int[] cutTheSticks(int[] arr) {
+    List<Integer> sticksToCut = Arrays.stream(arr).boxed().sorted().collect(Collectors.toList());
+    List<Integer> result = new ArrayList<>();
 
+    while (sticksToCut.size() != 0) {
+      result.add(sticksToCut.size());
+      int smallestStick = sticksToCut.get(0);
+      sticksToCut = sticksToCut.stream().map(x -> x - smallestStick).filter(x -> x != 0)
+          .collect(Collectors.toList());
+    }
+
+    return result.stream().mapToInt(Integer::intValue).toArray();
+
+  }
+
+  public static void main(String[] args) {
+    cutTheSticks(new int[]{5, 4, 4, 2, 2, 8});
   }
 
 }
