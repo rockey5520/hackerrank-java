a6c8ee4a900f96c4003c6e62a2d47b15be40b863
Rakesh Mothukuri <rockey5520@gmail.com>
2020-06-26 09:50:35 +0100

    Angry Professor Solution.java and README.md

---
+++ algorithms/implementation/angryprofessor/Solution.java
@@ -0,0 +1,13 @@
+package implementation.angryprofessor;
+
+public class Solution {
+
+  static String angryProfessor(int k, int[] a) {
+
+  }
+
+  public static void main(String[] args) {
+
+  }
+
+}
