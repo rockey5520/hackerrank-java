92155f00ea09e47b1a9d4348824710518dda96a4
Rakesh Mothukuri <rockey5520@gmail.com>
2020-06-26 15:33:15 +0100

    Find Digit Solution.java and README.md

--- algorithms/implementation/finddigit/Solution.java
+++ algorithms/implementation/finddigit/Solution.java
@@ -1,9 +1,25 @@
 package implementation.finddigit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
 
-  public static void main(String[] args) {
+  static int findDigits(int n) {
+    int value = n;
+    List<Integer> integers = new ArrayList<>();
+    while (n > 0) {
+      int a = n % 10;
+      if (a != 0 && value % a == 0) {
+        integers.add(a);
+      }
+      n /= 10;
+    }
+    return integers.size();
+  }
 
+  public static void main(String[] args) {
+    System.out.println(findDigits(1012));
   }
 
 }
