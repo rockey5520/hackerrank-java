3f62f4bc267ad5105d63136fee2f9e66f8769fbe
Rakesh Mothukuri <rockey5520@gmail.com>
2020-06-26 14:10:54 +0100

    Sequence Equation Solution.java and README.md

--- algorithms/implementation/sequenceequation/Solution.java
+++ algorithms/implementation/sequenceequation/Solution.java
@@ -2,8 +2,22 @@ package implementation.sequenceequation;
 
 public class Solution {
 
-  public static void main(String[] args) {
+  static int[] permutationEquation(int[] p) {
+    final int[] indexes = new int[p.length];
+    for (int i = 0; i < indexes.length; i++) {
+      indexes[p[i] - 1] = i;
+    }
+    final int[] result = new int[p.length];
+    for (int i = 0; i < result.length; i++) {
+      result[i] = indexes[indexes[i]] + 1;
+    }
+    return result;
+  }
 
+
+  public static void main(String[] args) {
+    int[] ints = permutationEquation(new int[]{2, 3, 1});
+    //  System.out.println(Arrays.toString(ints));
   }
 
 }
