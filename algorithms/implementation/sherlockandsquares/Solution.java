fd43c1162841f832965299f1bb01db0fef097e24
Rakesh Mothukuri <rockey5520@gmail.com>
2020-06-26 16:13:31 +0100

    sherlock and squares Solution.java and README.md

--- algorithms/implementation/sherlockandsquares/Solution.java
+++ algorithms/implementation/sherlockandsquares/Solution.java
@@ -2,8 +2,12 @@ package implementation.sherlockandsquares;
 
 public class Solution {
 
-  public static void main(String[] args) {
+  static int squares(int a, int b) {
+    return (int) (Math.floor(Math.sqrt(b) - Math.ceil(Math.sqrt(a))) + 1);
+  }
 
+  public static void main(String[] args) {
+    System.out.println(squares(3, 9));
   }
 
 }
