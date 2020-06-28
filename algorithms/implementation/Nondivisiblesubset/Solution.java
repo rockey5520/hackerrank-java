5d5b0a342211eb2bd4683164b79044502933acce
Rakesh Mothukuri <rockey5520@gmail.com>
2020-06-28 08:57:29 +0100

    Non divisble subset Solution.java and README.md

--- algorithms/implementation/Nondivisiblesubset/Solution.java
+++ algorithms/implementation/Nondivisiblesubset/Solution.java
@@ -1,8 +1,35 @@
 package implementation.Nondivisiblesubset;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Solution {
 
-  public static void main(String[] args) {
+  public static int nonDivisibleSubset(int k, List<Integer> s) {
 
+    int[] objects = s.stream().mapToInt(Integer::intValue).toArray();
+
+    int[] f = new int[k];
+    Arrays.fill(f, 0);
+    for (int i = 0; i < objects.length; i++) {
+      f[objects[i] % k]++;
+    }
+    if (k % 2 == 0) {
+      f[k / 2] = Math.min(f[k / 2], 1);
+    }
+    int res = Math.min(f[0], 1);
+
+    for (int i = 1; i <= k / 2; i++) {
+      res += Math.max(f[i], f[k - i]);
+    }
+
+    return res;
+
+
+  }
+
+
+  public static void main(String[] args) {
+    nonDivisibleSubset(4, List.of(19, 10, 12, 10, 24, 25, 22));
   }
 }
