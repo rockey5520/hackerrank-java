e309ba570ade416701a62884f20b0f34167c5af9
Rakesh Mothukuri <rockey5520@gmail.com>
2020-07-06 19:28:03 +0100

    dynamic array

---
+++ datastructures/arrays/dynamicarray/Solution.java
@@ -0,0 +1,16 @@
+package arrays.dynamicarray;
+
+import java.util.List;
+
+public class Solution {
+
+  public static List<Integer> dynamicArray(int n, List<List<Integer>> queries) {
+    // Write your code here
+
+  }
+
+  public static void main(String[] args) {
+
+  }
+
+}
