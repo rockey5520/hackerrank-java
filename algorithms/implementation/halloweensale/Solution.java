f851348f8d8384c8b0592e2310bc2403b16d75d2
Rakesh Mothukuri <rockey5520@gmail.com>
2020-09-13 07:37:09 +0100

    Halloween Sale

--- algorithms/implementation/halloweensale/Solution.java
+++ algorithms/implementation/halloweensale/Solution.java
@@ -1,6 +1,32 @@
 package implementation.halloweensale;
 
+import java.util.Scanner;
+
 public class Solution {
 
+  static int howManyGames(int price, int d, int m, int initialAmount) {
+    int purchasesCount = 0;
+    while (initialAmount >= price) {
+      initialAmount -= price;
+      if (price - d >= m) {
+        price -= d;
+      } else {
+        price = m;
+      }
+      purchasesCount++;
+    }
+    return purchasesCount;
+  }
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int p = in.nextInt();
+    int d = in.nextInt();
+    int m = in.nextInt();
+    int s = in.nextInt();
+    int answer = howManyGames(p, d, m, s);
+    System.out.println(answer);
+    in.close();
+  }
 
 }
